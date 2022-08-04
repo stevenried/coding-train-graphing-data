26f25df71d09be8f73e9d4a9ceb894dccbaef436
Steven Ried <70533094+stevenried@users.noreply.github.com>
2022-08-04 12:27:05 -0400

    Make chart using chart.js library

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
+chartIt()
+
 async function getData() {
+  const xValues = []
+  const yValues = []
+
   const response = await fetch('ZonAnn.Ts+dSST.csv')
   const data = await response.text()
 
@@ -6,9 +11,42 @@ async function getData() {
   tableValues.forEach((row) => {
     const columns = row.split(',')
     const year = columns[0]
+    xValues.push(year)
     const meanTempDif = columns[1]
-    console.log(year, meanTempDif)
+    yValues.push(parseFloat(meanTempDif) + 14)
   })
+  return { xValues, yValues }
 }
 
-getData()
+async function chartIt() {
+  const data = await getData()
+  const ctx = document.querySelector('[data-id-chart]').getContext('2d')
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: data.xValues,
+      datasets: [
+        {
+          label:
+            'Combined Land-Surface Air and Sea-Surface Water Temperature Anomalies (°C) from 1880',
+          data: data.yValues,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          pointRadius: 0,
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: 'Combined Land-Surface Air and Sea-Surface Water Temperature Anomalies (°C) from 1880',
+        },
+        legend: {
+          display: false,
+        },
+      },
+    },
+  })
+}
